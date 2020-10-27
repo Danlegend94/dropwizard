800af5d22ee4bb9fe928764d7ea8f7307da0e7f2
daniele.spiga <daniele.spiga@realt.it>
2020-10-27 11:08:52 +0100

    add test for method greetSecurity

--- src/test/java/com/corso/dropwizard/udemy/dropbookamrksprova/resources/HelloResourceTest.java
+++ src/test/java/com/corso/dropwizard/udemy/dropbookamrksprova/resources/HelloResourceTest.java
@@ -1,24 +1,63 @@
 package com.corso.dropwizard.udemy.dropbookamrksprova.resources;
 
-
-import org.junit.ClassRule;
-import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
 
 import javax.ws.rs.core.MediaType;
+
+import org.eclipse.jetty.security.authentication.BasicAuthenticator;
+import org.glassfish.jersey.client.authentication.HttpAuthenticationFeature;
+import org.glassfish.jersey.test.grizzly.GrizzlyWebTestContainerFactory;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+import com.corso.dropwizard.udemy.dropbookamrksprova.core.User;
+import com.google.common.base.Optional;
+
+import io.dropwizard.auth.AuthFactory;
+import io.dropwizard.auth.AuthenticationException;
+import io.dropwizard.auth.Authenticator;
+import io.dropwizard.auth.basic.BasicAuthFactory;
+import io.dropwizard.auth.basic.BasicCredentials;
 import io.dropwizard.testing.junit.ResourceTestRule;
 
 public class HelloResourceTest {
-	
+
+	private static HttpAuthenticationFeature FEATURE = HttpAuthenticationFeature.basic("username", "password");
+
+	public static final Authenticator<BasicCredentials, User> SECURITYRULE = new Authenticator<BasicCredentials, User>() {
+
+		@Override
+		public Optional<User> authenticate(BasicCredentials credentials) throws AuthenticationException {
+			return Optional.of(new User());
+		}
+	};
+
 	@ClassRule
-	public static final ResourceTestRule RULE = new ResourceTestRule.Builder().addResource(new HelloResources()).build();
-	
+	public static final ResourceTestRule RULE = new ResourceTestRule.Builder()
+			.addProvider(AuthFactory.binder(new BasicAuthFactory<>(SECURITYRULE, "Security", User.class)))
+			.addResource(new HelloResources())
+			.setTestContainerFactory(new GrizzlyWebTestContainerFactory())
+			.build();
+
 	@Test
 	public void HelloResource() {
 		String expected = "Hello world";
 		String actual = RULE.getJerseyTest().target("/hello").request(MediaType.TEXT_PLAIN_TYPE).get(String.class);
 		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void HelloResourceSecurity() {
+		String expected = "Hello world security";
+		String actual = RULE.getJerseyTest().target("/hello/security").request(MediaType.TEXT_PLAIN_TYPE)
+				.get(String.class);
+		assertEquals(expected, actual);
+	}
+	
+	@BeforeClass
+	public static void setUpClass() {
+		RULE.getJerseyTest().client().register(FEATURE);
 	}
 
 }
